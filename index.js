f8b48170f4556ec859cc21e4adcb0d7184c539fa
Frank  D'Angelo <08fdange@gmail.com>
2020-01-22 01:20:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function  add() {
+function  add(a,b) {
   return a + b
 }
 
-function(subtract)
+function subtract(a,b) {
+  return a - b 
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b 
+}
+
+function inc(n)
