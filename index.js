9467fc0586550508177b7051fb66d03752b9ef17
Frank  D'Angelo <08fdange@gmail.com>
2020-01-22 01:28:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  return n++
+  return ++n
 }
 
 function dec(n) {
-  return n--
+  return --n
 }
 
 function makeInt(n) {
-  returnparseInt(n, 10)
+  return parseInt(n, 10)
 }
 
 function preserveDecimal(n) {
