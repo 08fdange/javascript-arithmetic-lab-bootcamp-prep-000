bad4f1abbcd46a54b6e6b1afb2f8111aaebe3310
Frank  D'Angelo <08fdange@gmail.com>
2020-01-22 01:27:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-  return(a * b)
+  return a * b
 }
 
 function divide(a, b) {
-  return(a / b)
+  return a / b 
 }
 
 function inc(n) {
-  return(n ++)
+  return n++
 }
 
 function dec(n) {
-  return(n --)
+  return n--
 }
 
 function makeInt(n) {
-  return(parseInt(n, 10))
+  returnparseInt(n, 10)
 }
 
 function preserveDecimal(n) {
-  return(parseFloat(n))
+  return parseFloat(n)
 }
 
